8eec84ad480705fbd101fcf406c323947efd85c1
0xsky <103289721+tegu03@users.noreply.github.com>
2025-06-21 10:09:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,29 @@
+# config.py
+
+# Playlist YouTube untuk sumber sound
+PLAYLIST_URL = "https://www.youtube.com/playlist?list=YOUR_PLAYLIST_ID"
+
+# Daftar karakter yang tersedia di folder karakter/
+KARAKTER_LIST = [
+    "upin",
+    "ipin",
+    "tok_dalang",
+    "ah_tong",
+    "angkel_muthu",
+    "opah",
+    "dontol",
+    "tung_tung_sahur"
+]
+
+# Keyword yang akan digunakan untuk hook & hashtag
+KEYWORDS = [
+    "UPIN IPIN", "DONTOL", "Tung Tung Sahur", "Ah Tong",
+    "Tok Dalang", "Angkel Muthu", "Opah"
+]
+
+# Font untuk teks cerita di video
+FONT_PATH = "assets/font.ttf"
+
+# Durasi video maksimal (dalam detik)
+MAX_DURATION = 15
 
